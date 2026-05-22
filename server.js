15a413bc08a0de2425730fcfc1f67ebdd378173e
SimonePalmisciano <simonepalmisciano11@gmail.com>
2026-05-22 15:18:57 +0200

    importate foto e data posts, e creato una rotta bacheca che restituisce il json dei post

--- server.js
+++ server.js
@@ -1,8 +1,26 @@
-import express from express;
+import express from 'express';
+import { posts } from './data/posts.js';
 
 const app = express();
 const url = process.env.SERVER_URL;
-const port = process.env.SERVER_PORT;
+const port = process.env.SERVER_PORT || 3000;
+
+const messaggioOk = {
+    messaggio: 'va tutto bene nel server'
+};
+
+app.get("/", (request, response) => {
+    response.json(messaggioOk)
+});
+
+app.get("/bacheca", (request, response) => {
+    response.json(posts.map(post => {
+        return {
+            ...post,
+            img: `http://${url}:${port}/bacheca/${post.img}`,
+        }
+    }));
+})
 
 app.listen(port, (error) => {
     if (error) {
@@ -10,5 +28,5 @@ app.listen(port, (error) => {
         return;
     }
     console.log('server in ascolto a questa porta: ', port);
-    
+
 });
